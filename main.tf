fdc519ef6a8c47e0d1321757c7f35d9c107e2041
Kamsani1973 <sanib@live.com>
2025-07-09 01:23:24 +0800

    Add terraform workflow

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "ap-southest-1"  
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key = "github-demo.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "name" {
+     bucket_prefix =  "kamsani-github-demo-bucket"
+}  
